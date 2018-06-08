d541f9c78b61496f1df14fb2a7c4584187dde7c4
Reuel Bigo <2121855@slu.edu.ph>
2018-06-08 14:44:55 +0800

    yhe

--- application/views/admin/fragments/projectPlanNavigation.php
+++ application/views/admin/fragments/projectPlanNavigation.php
@@ -3,7 +3,6 @@
       <div class="row">
         <div class="page-title">
           <div class="title_left">
-            <h4>Infrastructure Projects Procurement Monitoring and Information System <?php echo date('Y');?></h4>
           </div>
         </div>
       </div>
