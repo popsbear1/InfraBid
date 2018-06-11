c9d5f3448350462a81c4fbb7beb66192daf140f1
zerocokes <jaysoncaliway@gmail.com>
2018-06-11 17:40:12 +0800

    UI modified for document tracking

--- application/views/doctrack/docTrack.php
+++ application/views/doctrack/docTrack.php
@@ -1,78 +1,134 @@
-<div class="col-md-12 col-sm-12 col-lg-12">
-  <div class="x_panel">
-    <div class="x_title">
-      <h2>Document Tracking</h2>
-      <ul class="nav navbarQ-right panel_toolbox">
-        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Settings 1</a>
-            </li>
-            <li><a href="#">Settings 2</a>
-            </li>
-          </ul>
-        </li>
-        <li><a class="close-link"><i class="fa fa-close"></i></a>
-        </li>
-      </ul>
-      <div class="clearfix"></div>
+
+        <!-- page content -->
+    <div class="form-group no-print">
+      <div class="col-md-3 col-sm-6 col-xs-6 col-md-offset-9">
+        <a href="<?php echo base_url('admin/addFundsView') ?>" type="button" class="btn btn-primary">Add New Document</a>
+      </div>
     </div>
-    <div class="x_content">
-
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
-
+    <div class="clearfix"></div>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2>Document Tracking</h2>
+            <ul class="nav navbar-right panel_toolbox noPrint">
+              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Settings 1</a>
+                  </li>
+                  <li><a href="#">Settings 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li><a class="close-link"><i class="fa fa-close"></i></a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            <table class="datatable-1 table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th class="text-center">DEPT 1</th>
+                  <th class="text-center">DEPT 2</th>
+                  <th class="text-center">DEPT 3</th>
+                  <th class="text-center">DEPT 4</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
 
-<!-- /page content -->
+    <!-- jQuery -->
+    <script src="<?php echo base_url() ?>public/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?php echo base_url() ?>public/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="<?php echo base_url() ?>public/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="<?php echo base_url() ?>public/vendors/nprogress/nprogress.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="<?php echo base_url() ?>public/build/js/custom.min.js"></script>
+    <!-- Datatables -->
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/datatables.net-scroller/js/datatables.scroller.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/jszip/dist/jszip.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/pdfmake/build/pdfmake.min.js"></script>
+    <script src="<?php echo base_url() ?>public/vendors/pdfmake/build/vfs_fonts.js"></script>
 
-</div>
-</div>
+     <script>
+        $(document).ready(function() {
+        var handleDataTableButtons = function() {
+          if ($(".datatable-1").length) {
+            $(".datatable-1").DataTable({
+              dom: "Bfrtip",
+              lengthMenu: [
+            [ 10, 25, 50, -1 ],
+            [ '10 rows', '25 rows', '50 rows', 'Show all' ]
+                         ],
 
-<!-- jQuery -->
-<script src="<?php echo base_url() ?>public/vendors/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap -->
-<script src="<?php echo base_url() ?>public/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- FastClick -->
-<script src="<?php echo base_url() ?>public/vendors/fastclick/lib/fastclick.js"></script>
-<!-- NProgress -->
-<script src="<?php echo base_url() ?>public/vendors/nprogress/nprogress.js"></script>
-<!-- iCheck -->
-<script src="<?php echo base_url() ?>public/vendors/iCheck/icheck.min.js"></script>
+              buttons: [
 
-<!-- Custom Theme Scripts -->
-<script src="<?php echo base_url() ?>public/build/js/custom.min.js"></script>
-</body>
-</html>
+                'pageLength',
+            {
+                extend: 'copy',
+            },
+
+            {
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: [ 0, 1]
+                }
+            },
+
+            {
+                extend: 'pdfHtml5',
+                exportOptions: {
+                    columns: [ 0, 1]
+                }
+            },
+
+            {
+                extend: 'print',
+                exportOptions: {
+                    columns: [ 0, 1]
+                }
+            }
+              ],
+              responsive: true
+            });
+          }
+        };
+
+        TableManageButtons = function() {
+          "use strict";
+          return {
+            init: function() {
+              handleDataTableButtons();
+            }
+          };
+        }();
+
+        TableManageButtons.init();
+      });
+    </script>
