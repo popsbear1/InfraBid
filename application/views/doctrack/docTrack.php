b6ee4d6f93cd3b331d308eeef51fedb2c9b7acd8
Jayson <jaysoncaliway@gmail.com>
2018-06-26 12:09:26 +0800

    Modified User interface for Document Tracking.

--- application/views/doctrack/docTrack.php
+++ application/views/doctrack/docTrack.php
@@ -3,54 +3,16 @@
     </div>
     <div class="clearfix"></div>
     <div class="row">
-      <nav class="navbar navbar-default" style="background: orange">
-        <div class="container-fluid">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <a class="navbar-brand" href="#">Annual Procurement Plan </a>
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-
-          </div>
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <ul class="nav navbar-nav navbar-centered collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <li><a href="">Secretary</a></li>
-            <li ><a href="">BAC-INFRA</a></li>
-            <li ><a href="">PGO</a></li>
-            <li ><a href="">PEO</a></li>
-          </ul>
-        </div><!-- /.container-fluid -->
-      </nav>
-      
     </div>
     <div class="row">
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
           <div class="x_title">
             <h2>Document Tracking</h2>
-            <ul class="nav navbar-right panel_toolbox noPrint">
-              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-              </li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                <ul class="dropdown-menu" role="menu">
-                  <li><a href="#">Settings 1</a>
-                  </li>
-                  <li><a href="#">Settings 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li><a class="close-link"><i class="fa fa-close"></i></a>
-              </li>
-            </ul>
             <div class="clearfix"></div>
           </div>
           <div class="x_content">
-            <table class="datatable-1 table table-striped table-bordered">
+            <table class="table table-striped table-bordered">
               <thead>
                 <tr>
                   <th class="text-center">Project ID</th>
@@ -62,12 +24,12 @@
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td></td>  
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                <tr class="text-center">
+                  <td>sample</td>  
+                  <td>sample</td>
+                  <td>sample</td>
+                  <td>sample</td>
+                  <td>sample</td>
                   <td class="text-center">
                     <form action=" <?php echo base_url("doctrack/documentDetailsView") ?>">
                       <button class="btn btn-success" type=""
