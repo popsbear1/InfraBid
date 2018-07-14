5749702674381b8d4a07373279df6c7191369da7
zerocokes <jaysoncaliway@gmail.com>
2018-07-14 14:23:20 +0800

    removed old document track

--- application/views/admin/fragments/nav.php
+++ application/views/admin/fragments/nav.php
@@ -333,12 +333,6 @@
           </li>
           <li>
             <a href="<?php echo base_url('doctrack/docTrackView') ?>">
-              <i class="fa fa-file"></i> <span>Doc Track</span>
-            </a>
-          </li>
-          <li class="header">Document</li>
-          <li>
-            <a href="<?php echo base_url('doctrack/documentTrackHomeView') ?>">
               <i class="fa fa-file"></i> <span>Document Track</span>
             </a>
           </li>
