a449029cdbfe049ba30c321de990d9ee8a7f6c8d
Jayson <jaysoncaliway@gmail.com>
2018-07-15 23:16:11 +0800

    ui added

--- application/views/doctrack/docTrack.php
+++ application/views/doctrack/docTrack.php
@@ -200,27 +200,58 @@
 <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" id="documentDetailsViewModal">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
       <div class="modal-body">
-        <form>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Recipient:</label>
-            <input type="text" class="form-control" id="recipient-name">
+        <section class="content">
+          <div class="row">
+            <div class="col-lg-12 col-sm-12 col-xs-12 col-md-12">
+              <div class="box text-center">
+                <div class="box-header">
+                  <h2 class="box-title">
+                    HISTORY TRACKS
+                  </h2>
+                </div>
+              <div class="box-body">
+              <table class="table table-striped table-bordered" id="projectDocumenTable">
+                <thead>
+                  <div class="row">
+                    <tr>
+                      <th class="text-center">RECEIVING</th>
+                    <tr>
+                      <th class="text-center">Department</th>
+                      <th class="text-center">Date/Time Received</th>
+                      <th class="text-center">Received By</th>
+                      <th class="text-center">Remarks</th>
+                      <th class="text-center">Status</th>
+                      <th class="text-center">Date/Time Received</th>
+                      <th class="text-center">Forwarded By</th>
+                      <th class="text-center">Forwarded To</th>
+                      <th class="text-center">Forward (Remarks)</th>
+                    </tr>
+                  </div>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>                         
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="message-text" class="col-form-label">Message:</label>
-            <textarea class="form-control" id="message-text"></textarea>
-          </div>
-        </form>
+        </div>
       </div>
+    </section>      
+  </div>
+
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
       </div>
     </div>
   </div>
--- application/views/doctrack/fragments/docTrackNavigation.php
+++ application/views/doctrack/fragments/docTrackNavigation.php
@@ -5,7 +5,7 @@
           <nav class="navbar navbar-default">
             <div class="container-fluid">
               <!-- Brand and toggle get grouped for better mobile display -->
-              <a class="navbar-brand" href="#">Document Tracking</a>
+              <a class="navbar-brand" href="">Document Tracking</a>
               <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                   <span class="sr-only">Toggle navigation</span>
@@ -17,6 +17,7 @@
               <!-- Collect the nav links, forms, and other content for toggling -->
               <ul class="nav navbar-nav navbar-centered collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <li><a href="<?php echo base_url('doctrack/historyView') ?>">History</a></li>
+                <li><a href="<?php echo base_url('doctrack/documentsDetailsView') ?>">Managing Documents</a></li>
               </ul>
             </div><!-- /.container-fluid -->
           </nav>
