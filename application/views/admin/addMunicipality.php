f42e67534462933fe48a03c109c584ecd9c710f6
Jayson <jaysoncaliway@gmail.com>
2018-07-25 09:59:38 +0800

    municipyo

--- application/views/admin/addMunicipality.php
+++ application/views/admin/addMunicipality.php
@@ -18,22 +18,28 @@
             <div class="alert alert-warning">
               <p><?php echo $_SESSION['error'] ?></p>
             </div>
-          <?php endif ?>
+            <?php endif ?>
+          <div class="alert alert-success text-center" id="adding_success" hidden>
+            <p class="text-left"><b>SUCCESS!</b></p>
+            <p>The Municipality and Barangay has been added!</p>
+          </div>
+          <div class="alert alert-warning text-center" id="adding_failed" hidden>
+            <p class="text-left"><b>FAILED!</b></p>
+            <p>An error was encountered. The Operation has been failed!</p>  
+          </div>
           <form id="addMunicipalityForm" method="POST" data-parsley-validate class="form-horizontal form-label-left" action="<?php echo base_url('admin/addNewMunicipality') ?>">
-
             <div class="form-group">
-              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="municipality_code">Municipality Code<span class="required">*</span>
+              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="municipality_code">Municipality Code
               </label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" step="any"  id="municipality_code" value="" name="municipality_code"  required="required" class="form-control col-md-7 col-xs-12">
+                <input type="text" step="any"  id="municipality_code" value="" name="municipality_code"   class="form-control col-md-7 col-xs-12">
               </div>
             </div>
-
             <div class="form-group">
-              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="municipality">Name of Municipality<span class="required">*</span>
+              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="municipality">Name of Municipality
               </label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" step="any"  id="municipality" value="" name="municipality"  required="required" class="form-control col-md-7 col-xs-12">
+                <input type="text" step="any"  id="municipality" value="" name="municipality" class="form-control col-md-7 col-xs-12">
               </div>
             </div>
             <div class="ln_solid"></div>
@@ -44,7 +50,7 @@
                     <div class="form-group">
                       <label class="col-lg-6 control-label">Number Of Barangay/s</label>
                       <div class="col-lg-6">
-                        <input type="number" min="0" class="form-control" id="barangayNumber">
+                        <input type="number" min="0" class="form-control" id="barangayNumber" name="barangayNumber">
                       </div>
                     </div>
                   </div>
@@ -63,7 +69,7 @@
             <div class="ln_solid"></div>
             <div class="form-group">
               <div class="col-lg-12 text-center">
-                <button href="#myModal" type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Submit</button>
+                <button type="submit" class="btn btn-primary" form="addMunicipalityForm">Submit</button>
               </div>
             </div>
           </form>
@@ -72,39 +78,6 @@
     </div>
   </div>
   <!-- modal for data confirmation -->
-  <div id="myModal" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
-          </button>
-          <h4 class="modal-title" id="myModalLabel">Confirm Input Values</h4>
-        </div>
-        <div class="modal-body">
-          <table class='table table-striped table-bordered' style='font-size:13px;'>
-            <thead>
-              <tr >
-                <th style='text-align: center'>Attributes</th>
-                <th style='text-align: center'>Values</th>
-              </tr> 
-            </thead>
-            <tbody>
-              <tr><td>Municipality Code</td>
-                <td><span id="code"></span></td>
-              </tr>
-              <tr><td>Municipality</td>
-                <td><span id="name"></span></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="submit" form="addMunicipalityForm" name="submit" class="btn btn-primary">Confirm</button>
-        </div>
-      </div>
-    </div>
-  </div>
   <!-- end of modal -->
 </section>
 
@@ -198,3 +171,39 @@
   })
 </script>
 
+<script>
+  $('#addMunicipalityForm').submit(function(e){
+    e.preventDefault();
+
+    $.ajax({
+      type: 'POST',
+      url: $('#addMunicipalityForm').attr('action'),
+      data: $('#addMunicipalityForm').serialize(),
+      dataType: 'json',
+      success: function(response){
+        if (response.success == true) {
+          $('.has-error').remove();
+          $('.has-success').remove();
+          $('#alert-success').prop('hidden', false);
+          $('.alert-success').delay(500).show(10, function() {
+          $(this).delay(3000).hide(10, function() {
+            $(this).remove();
+          });
+          })
+        }else{
+          $.each(response.messages, function(key, value) {
+            var element = $('#' + key);
+            
+            element.closest('div.form-group')
+            .removeClass('has-error')
+            .addClass(value.length > 0 ? 'has-error' : 'has-success')
+            .find('.text-danger')
+            .remove();
+            
+            element.after(value);
+          });
+        }
+      }
+    });
+  })
+</script>
