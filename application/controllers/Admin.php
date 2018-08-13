56fe2d926a5d03e5a388e5ce79326f6fe3350d3f
jayson <jaysoncaliway@gmail.com>
2018-08-13 14:12:15 +0800

    adik ammu experiment

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1709,16 +1709,11 @@ class Admin extends CI_Controller {
 		redirect('admin/manageMunicipalitiesAndBarangays');	
 	}
 
-	public function fpdf(){
+	public function fpdfView(){
 		$this->load->library('CustomFPDF');
 		$pdf = $this->customfpdf->getInstance();
-
 		$pdf->AliasNbPages();
-		$pdf->AddPage();
-		$pdf->header('Arial');
-		$pdf->SetFont('Times','',12);
-		for($i=1;$i<=40;$i++)
-		    $pdf->Cell(0,10,'Printing line number '.$i,0,1);
+		$pdf->AddPage('L');
 		$pdf->Output(); 
 	}
 }
--- application/libraries/CustomFPDF.php
+++ application/libraries/CustomFPDF.php
@@ -5,15 +5,54 @@ require(APPPATH . 'third_party/fpdf181/fpdf.php');
 
 class CustomFPDF extends FPDF {
 
-    public function header($family){
-        $this->SetFont($family,'B',15);
+
+    public function header(){
+        $this->SetFont('Arial','B',10);
         $this->Cell(80);
-        $this->Cell(30,10,'Title',1,0,'C');
+        $this->Cell(30,10,'Republic of The Philippines',0,0,'C');
+        $this->Cell(50,10,'Benguet Provincial Government',0,0,'C');
+        $this->Cell(70,10,'Annual Procurement Plan',0,0,'C');
         $this->Ln(20);
+
+        $this->Cell(40,5,' ','LTR',0,'L',0);   // empty cell with left,top, and right borders
+		$this->Cell(40,5,'NO.','LR',0,'C',0);
+		$this->Cell(40,5,'PROJ. NO.','LR',0,'C',0);
+		$this->Cell(40,5,'PROCUREMENT PROGRAMS / PROJECTS','LR',0,'C',0);
+		$this->Cell(40,5,'Solid Here','LR',0,'C',0);
+		$this->Cell(40,5,'MODE OF PROCUREMENT','LR',0,'C',0);
+
+
+		                $this->Ln();
+
+		$this->Cell(40,5,'Solid Here','LR',0,'C',0);  // cell with left and right borders
+		$this->Cell(50,5,'[ o ] che1','LR',0,'L',0);
+		$this->Cell(50,5,'[ x ] che2','LR',0,'L',0);
+
+		                $this->Ln();
+
+		$this->Cell(40,5,'','LBR',0,'L',0);   // empty cell with left,bottom, and right borders
+		$this->Cell(50,5,'[ x ] def3','LRB',0,'L',0);
+		$this->Cell(50,5,'[ o ] def4','LRB',0,'L',0);
+
+                $this->Ln();
+                $this->Ln();
+                $this->Ln();
     }
+
+
+    public function Footer(){
+	    // Position at 1.5 cm from bottom
+	    $this->SetY(-15);
+	    // Arial italic 8
+	    $this->SetFont('Arial','I',8);
+	    // Page number
+	    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+	}
 
     public function getInstance(){
         return new CustomFPDF();
     }
+
+
 }
 ?>
