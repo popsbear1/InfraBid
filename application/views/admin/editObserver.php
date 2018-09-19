53ea012dbbcca6354ee1f8cfc44baf1e6e9cd9e5
Jayson <jaysoncaliway@gmail.com>
2018-09-19 14:53:24 +0800

    edit observer does not work

--- application/views/admin/editObserver.php
+++ application/views/admin/editObserver.php
@@ -1,8 +1,8 @@
 
 <section class="content">
   <div class="row">
-    <div class="col-lg-12 col-sm-12 col-md-12">
-      <h3 class="pull-left">Edit Observer Detail</h3>
+    <div class="col-md-12">
+      <h3 class="pull-left">Edit Observer Details</h3>
     </div>
   </div>
   <div class="row">
@@ -16,7 +16,7 @@
   <div class="box">
     <div class="box-header">
       <h2 class="box-title">Edit Observers<small></small></h2>
-    </div> 
+    </div>
     <div class="box-body">
       <?php if (isset($_SESSION['success'])): ?>
         <div class="alert alert-success text-center">
@@ -28,22 +28,23 @@
           <p><?php echo $_SESSION['error'] ?></p>
         </div>
       <?php endif ?>
-      <form id="editDocumentsForm" method="POST" class="form-horizontal" action="<?php echo base_url('admin/editObservers') ?>">
+      <form id="editContractorDetailsForm" method="POST" class="form-horizontal" action="<?php echo base_url('admin/editObservers') ?>" autocomplete="off" >
+
         <div class="form-group">
-          <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Department Name/Observer<span class="required">*</span>
+          <label class="control-label col-md-3 col-sm-3 col-xs-12">Observer Department Name*</span>
           </label>
           <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" step="any"  id="observer_dept_name" name="observer_dept_name" class="form-control col-md-7 col-xs-12" placeholder="<?php echo $observerDetail['observer_dept_name'] ?>">
+            <input type="text" step="any"  id="businessname" value="" name="businessname" class="form-control col-md-7 col-xs-12" placeholder="<?php echo $observerDetail['observer_dept_name'] ?>">
+            </div>
           </div>
         </div>
       </form>
     </div>
     <div class="box-footer text-center">
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editDocumentModal">Submit</button> 
+      <button href="#myModal" type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Submit</button>
     </div>
   </div>
 </section>
-
 
 <script src="<?php echo base_url() ?>public/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
@@ -77,54 +78,48 @@
 <script src="<?php echo base_url() ?>public/bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url() ?>public/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo base_url() ?>public/dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url() ?>public/dist/js/demo.js"></script>
 <!-- DataTables -->
 <script src="<?php echo base_url() ?>public/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="<?php echo base_url() ?>public/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 
 <script>
   $(document).ready(function() {
-    $('#editDocumentModal').on('show.bs.modal' , function (e) {
-      $('#documentnumber').html($('#observer_dept_name').val());
+    $('#myModal').on('show.bs.modal' , function (e) {
+      $('#usernam').html($('#businessname').val());
     });
   });
 </script>
 
 
-    <!-- modal for data confirmation -->
-    <div id="editDocumentModal" class="modal fade" role="dialog" aria-hidden="true">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
-            </button>
-            <h4 class="modal-title" id="myModalLabel">Confirm Input Values</h4>
-          </div>
-          <div class="modal-body">
-            <table class='table table-striped table-bordered' style='font-size:13px;'>
-              <thead>
-                <tr >
-                  <th style='text-align: center'>Attributes</th>
-                  <th style='text-align: center'>Values</th>
-                </tr> 
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Department Name/Observer</td>
-                  <td><span id="documentnumber"></span></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="submit" form="editDocumentsForm" name="submit" class="btn btn-primary">Confirm</button>
-          </div>
-        </div>
+<!-- modal for data confirmation -->
+<div id="myModal" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+        </button>
+        <h4 class="modal-title" id="myModalLabel">Confirm Input Values</h4>
+      </div>
+      <div class="modal-body">
+        <table class='table table-striped table-bordered' style='font-size:13px;'>
+          <thead>
+            <tr >
+              <th style='text-align: center'>Attributes</th>
+              <th style='text-align: center'>Values</th>
+            </tr> 
+          </thead>
+          <tbody>
+            <tr><td>Observer Department Name</td>
+              <td><span id="usernam"></span></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      <button type="submit" form="editContractorDetailsForm" name="submit" class="btn btn-primary">Confirm</button>
       </div>
     </div>
-    <!-- end of modal -->
+  </div>
+</div>
+<!-- end of modal -->
