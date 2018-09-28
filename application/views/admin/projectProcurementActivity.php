20d6d26e51b14bf138091ba2bfc53fcddbba136b
Reuel <bigoreuel.rb@gmail.com>
2018-09-27 20:56:52 -0800

    kasjy paylang

--- application/views/admin/projectProcurementActivity.php
+++ application/views/admin/projectProcurementActivity.php
@@ -819,24 +819,27 @@ function convertDate($date){
           </div>
           <div class="modal-footer">
               <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-default" id="smallModal">Confirm</button>
+              <button type="button" class="btn btn-default" data-toggle="modal" data-target="#smallModal">Confirm</button>
           </div>
         </div>
       </div>
     </div>
 
       <div id="smallModal" class="modal fade" role="dialog">
+        <div class="modal-dialog modal-sm" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-              <h4 class="modal-title">Confirmation</h4>
+            <h5 class="modal-title">Confirm</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;
+              </button>
             </div>
             <div class="modal-body">
-              <p>Are you sure you chose the right bidders?</p>
+              <p>Proceed with selected biders?</p>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               <button type="submit" class="btn btn-primary" form="selected_contractors_form">Confirm</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             </div>
           </div>
         </div>
